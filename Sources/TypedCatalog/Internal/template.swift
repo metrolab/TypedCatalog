eb53a702f58d95ec0871de9c691219b7b88ad301
Hèctor Marquès <h@mrhector.me>
2019-11-20 17:41:00 +0100

    Add support to main bundle

--- Sources/TypedCatalog/Internal/template.swift
+++ Sources/TypedCatalog/Internal/template.swift
@@ -1,38 +1,53 @@
 import Foundation
 
 func template(enumName: String, cases: String, associatedClass: String, bundleName: String) -> String {
-    let file =
-"""
-// Generated with Misen by tasanobu - Modules/CommonUI/Scripts/misen.swift
-// DO NOT EDIT
-
-import UIKit
-
-public enum \(enumName): String {
-\(cases)
-    private static let bundle: Bundle? = {
-        guard let path = Bundle(for: \(associatedClass).self).path(forResource: "\(bundleName)", ofType: "bundle") else { return nil }
-        return Bundle(path: path)
+    assert(associatedClass.isEmpty == bundleName.isEmpty, "If associated class is empty, bundle name should also be empty.")
+    let bundle: String = {
+        if associatedClass.isEmpty || bundleName.isEmpty {
+            return """
+                    return Bundle.main
+            """
+        } else {
+            return """
+                    guard let path = Bundle(for: \(associatedClass).self).path(forResource: "\(bundleName)", ofType: "bundle") else {
+                        return nil
+                    }
+                    return Bundle(path: path)
+            """
+        }
     }()
 
-    private func imageWithRenderingMode(_ mode: UIImage.RenderingMode) -> UIImage? {
-        guard let bundle = \(enumName).bundle else { return nil }
-        // swiftlint:disable:next image_init
-        let image = UIImage(named: self.rawValue, in: bundle, compatibleWith: nil)
-        return image?.withRenderingMode(mode)
+    let file =
+    """
+    // Generated with Misen by tasanobu - Modules/CommonUI/Scripts/misen.swift
+    // DO NOT EDIT
+
+    import UIKit
+
+    public enum \(enumName): String {
+    \(cases)
+        private static let bundle: Bundle? = {
+    \(bundle)
+        }()
+
+        private func imageWithRenderingMode(_ mode: UIImage.RenderingMode) -> UIImage? {
+            guard let bundle = \(enumName).bundle else { return nil }
+            // swiftlint:disable:next image_init
+            let image = UIImage(named: self.rawValue, in: bundle, compatibleWith: nil)
+            return image?.withRenderingMode(mode)
+        }
+
+        public var image: UIImage! {
+            return imageWithRenderingMode(.alwaysOriginal)
+        }
+
+        public var template: UIImage! {
+            return imageWithRenderingMode(.alwaysTemplate)
+        }
     }
 
-    public var image: UIImage! {
-        return imageWithRenderingMode(.alwaysOriginal)
-    }
+    private final class _TypedCatalog {}
 
-    public var template: UIImage! {
-        return imageWithRenderingMode(.alwaysTemplate)
-    }
-}
-
-private final class _TypedCatalog {}
-
-"""
+    """
     return file
 }
